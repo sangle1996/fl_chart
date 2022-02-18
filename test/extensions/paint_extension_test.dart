ce75164b239c185b218284d8b06e53a5c5395d9b
Iman Khoshabi <iman.neofight@gmail.com>
2022-02-18 22:04:39 +0330

    Write test for *_extensions.dart

--- test/extensions/paint_extension_test.dart
+++ test/extensions/paint_extension_test.dart
@@ -1,9 +1,5 @@
 import 'dart:ui';
 
-import 'package:fl_chart/src/chart/base/axis_chart/axis_chart_data.dart';
-import 'package:fl_chart/src/extensions/color_extension.dart';
-import 'package:fl_chart/src/extensions/edge_insets_extension.dart';
-import 'package:fl_chart/src/extensions/fl_titles_data_extension.dart';
 import 'package:fl_chart/src/extensions/paint_extension.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_test/flutter_test.dart';
